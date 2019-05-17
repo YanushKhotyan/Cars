250d07f7372816bbcd7808c6b57fccc6125b90e8
Degtyarev Kirill <kdegtyarev@marathonbet.ru>
2019-05-17 10:58:44 +0300

    kirill - c1

--- src/main/java/ws1/WorkShop.java
+++ src/main/java/ws1/WorkShop.java
@@ -1,4 +1,32 @@
 package ws1;
 
-public class WorkShop {
+
+import org.apache.log4j.Logger;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class WorkShop
+{
+    ArrayList<String> kList = new ArrayList<>();
+    HashMap<Integer, String> kHashMap = new HashMap<>();
+    Logger logger = Logger.getLogger(WorkShop.class);
+
+    @Test
+    public void wsTest2()
+    {
+        kList.add("Ferrary");
+        kList.add("Audi");
+        kList.set(1, "BMW");
+        kList.add(0,"Alfa");
+        assert kList.contains("BMW");
+        int ind = kList.indexOf("Ferrary");
+        logger.info("Index: " + ind);
+        logger.info("Array: " + kList.size());
+        logger.info("To array:" + Arrays.toString(kList.toArray()));
+
+
+    }
 }
